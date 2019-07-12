86cc1ece0b5adfd0c64daa514426eec6de442c1e
Braeden <braedenjury@gmail.com>
2019-07-11 22:54:07 -0700

    Basic OCR done

--- CookbookToGrocery/CookbookToGrocery.cpp
+++ CookbookToGrocery/CookbookToGrocery.cpp
@@ -14,6 +14,48 @@ int main()
 		return 1;
 	}
 
+	tess.SetPageSegMode(tesseract::PageSegMode::PSM_AUTO);
+	tess.SetVariable("save_best_choices", "T");
+
+	// read image
+	auto pixs = pixRead("test1.jpg");
+	if (!pixs)
+	{
+		std::cout << "Cannot open input file: " << "test.jpg" << std::endl;
+		return 1;
+	}
+
+	// recognize
+	tess.SetImage(pixs);
+	tess.Recognize(0);
+
+	// get result and delete[] returned char* string
+	char* raw_results = tess.GetUTF8Text();
+	std::string results = std::string(raw_results);
+	
+	std::vector<std::string> raw_ingredients;
+	int position = 0;
+	while (results.find('\n\n', position) != std::string::npos) {
+		raw_ingredients.push_back(results.substr(position, results.find('\n\n', position) - position));
+		position = results.find('\n\n', position) + 1;
+	}
+
+	for (int i = 0; i < raw_ingredients.size(); i++) {
+		std::cout << "Ingredient " << i << ": " << raw_ingredients[i] << std::endl;
+	}
+
+	//TODO: Determine lexer/parser for ingredients
+	//Some research needed.
+	
+
+	delete[] raw_results;
+
+
+	// cleanup
+	tess.Clear();
+	pixDestroy(&pixs);
+
+
     std::cout << "Hello World!\n"; 
 }
 
